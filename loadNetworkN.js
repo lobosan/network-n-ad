1c7732f94827661f791d58e4b959f3716a98f77f
Santiago Galindo <sp.galindoh@gmail.com>
2022-05-11 12:27:04 -0500

    Viafoura Integration

--- loadNetworkN.js
+++
@@ -1,8 +0,0 @@
-function loadNetworkN() {
-  const networkN = document.createElement("script");
-  networkN.setAttribute("async", true);
-  networkN.setAttribute("src", "https://kumo.network-n.com/dist/app.js");
-  networkN.setAttribute("site", "tech-raptor");
-  document.head.appendChild(networkN);
-  console.log("4. loadNetworkN");
-}
